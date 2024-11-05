4f45478a11bd069b998afe090dc1967f68e20854
Ksushik <loskutova7711@gmail.com>
2024-11-05 13:37:30 +0700

    pr18

---
+++ app/src/main/java/com/example/loskutova_pr22101_18/MainActivity.kt
@@ -0,0 +1,107 @@
+package com.example.loskutova_pr22101_18
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.loskutova_pr22101_18.ui.theme.Loskutova_pr22101_18Theme
+import org.json.JSONObject
+const val API_KEY = "012c748818fd41bbbdf112239221805"
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Loskutova_pr22101_18Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("London", this)
+                }
+            }
+        }
+    }
+}
+@Composable
+fun Greeting(name: String, context: Context) {
+    val state = remember {
+        mutableStateOf("Unknown")
+    }
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxHeight(0.5f)
+                .fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "Temperature in $name: ${state.value} Cº")
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            Button(onClick = {
+                getData(name, context, state)
+            }, modifier = Modifier.fillMaxWidth().padding(5.dp)) {
+                Text(text = "Refresh")
+            }
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Loskutova_pr22101_18Theme {
+
+    }
+}
+
+fun getData(name: String, context: Context, mState: MutableState<String>){
+    val url = "https://api.weatherapi.com/v1/current.json" +
+            "?key=$API_KEY&" +
+            "q=$name" +
+            "&aqi=no"
+    val queue = Volley.newRequestQueue(context)
+    val stringRequest = StringRequest(
+        Request.Method.GET,
+        url,
+        {
+                response->
+            val obj = JSONObject(response)
+            val temp = obj.getJSONObject("current")
+            mState.value = temp.getString("temp_c")
+            Log.d("MyLog","Response: ${temp.getString("temp_c")}")
+        },
+        {
+            Log.d("MyLog","Volley error: $it")
+        }
+    )
+    queue.add(stringRequest)
+}
+
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "loskutova_pr.22.101_18"
+include(":app")
+ 
